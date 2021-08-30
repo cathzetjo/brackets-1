76b75a3948e1004708ce8863677299fc19af7161
Katerina Zhadko <katerinkaj@icloud.com>
2021-08-30 22:49:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+    let strLength = str.length;
+    for (let i = 0; i < strLength; i++) {
+        let arrayLength = array.length;
+        const check = bracketsConfig.find(bracketsConfig => bracketsConfig.includes(str[i]));
+        if(check[0] === array[arrayLength - 1] && check[1]=== str[i]) {
+            array.pop();
+        } else {
+            array.push(str[i]);
+        }
+    }
+    return (array.length === 0);
+};
